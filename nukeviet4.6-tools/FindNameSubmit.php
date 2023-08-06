1762d459360afc240d07c0a08199f9c0f51e35d5
Phan Tấn Dũng <phantandung92@gmail.com>
2023-08-06 20:22:24 +0700

    Bổ sung tool

---
+++ nukeviet4.6-tools/FindNameSubmit.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * NUKEVIET Content Management System
+ * @version 5.x
+ * @author VINADES.,JSC <contact@vinades.vn>
+ * @copyright (C) 2009-2021 VINADES.,JSC. All rights reserved
+ * @license GNU/GPL version 2 or any later version
+ * @see https://github.com/nukeviet The NukeViet CMS GitHub project
+ */
+
+set_time_limit(0);
+
+function list_all_file($dir = '', $base_dir = '')
+{
+    $file_list = [];
+
+    if (is_dir($dir)) {
+        $array_filedir = scandir($dir);
+
+        foreach ($array_filedir as $v) {
+            if ($v == '.' or $v == '..') {
+                continue;
+            }
+
+            if (is_dir($dir . '/' . $v)) {
+                foreach (list_all_file($dir . '/' . $v, $base_dir . '/' . $v) as $file) {
+                    $file_list[] = $file;
+                }
+            } else {
+                // if( $base_dir == '' and ( $v == 'index.html' or $v == 'index.htm' ) ) continue; // Khong di chuyen index.html
+                if (preg_match('/\.(php|tpl|js)$/', $v) and !preg_match('/^\/?(data|vendor)\//', $base_dir . '/' . $v)) {
+                    $file_list[] = preg_replace('/^\//', '', $base_dir . '/' . $v);
+                }
+            }
+        }
+    }
+
+    return $file_list;
+}
+
+define('NV_ROOTDIR', str_replace('\\', '/', realpath(dirname(__FILE__) . '/')));
+
+$allfiles = list_all_file(NV_ROOTDIR);
+
+foreach ($allfiles as $filepath) {
+    $filecontents = file_get_contents(NV_ROOTDIR . '/' . $filepath);
+
+    if (preg_match('/name[\s]*\=[\s]*([\'\"\\\]*)submit([\'\"\\\]*)/isuU', $filecontents)) {
+        echo 'Check: ' . $filepath . "\n";
+    }
+}
+
+echo "OK\n";
---
+++ nukeviet4.6-tools/README.md
@@ -0,0 +1,5 @@
+# Các công cụ phục vụ phát triển NukeViet 4.6
+
+### FindNameSubmit.php
+
+Tìm các file có `name="submit"` cần đổi thành `name="fsubmit"` hoặc các giá trị khác. Copy file ngang hàng thư mục gốc và chạy `php FindNameSubmit.php`
